3893dbf48a668d8e904be8cb7d95fef9bff87ed4
Slavik <terakrokos@gmail.com>
2018-08-20 14:21:20 +0300

    Commit #2 was added

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
@@ -6,8 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
-
-
+export class AppComponent implements OnInit{
   title = 'app';
+  fullImagePath: string;
+
+  constructor() {
+    this.fullImagePath = './assets/images/dsz.jpg'
+  }
+
+  ngOnInit() {
+  }
+  
 }
